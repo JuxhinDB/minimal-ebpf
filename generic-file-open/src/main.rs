ee0907eebc3b04bbbe327ebefcc4d45f5b22de4c
Juxhin <juxhin@exein.io>
2022-06-08 23:45:56 +0100

    wip:get generic_file_open to work nicely

--- generic-file-open/src/main.rs
+++ generic-file-open/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     // load the BPF code
     let mut bpf = Bpf::load_file("../generic_file_open_example.o").unwrap();
 
-    // get the `ingress_filter` program compiled into `bpf.o`.
+    // get the program compiled into `bpf.o`.
     let program: &mut KProbe = bpf
         .program_mut("generic_file_open")
         .unwrap()
@@ -16,6 +16,8 @@ fn main() {
 
     // load the program into the kernel
     program.load().unwrap();
+
+    program.attach("generic_file_open", 0).unwrap();
 
     loop {}
 }
--- generic_file_open_example.c
+++ generic_file_open_example.c
@@ -4,8 +4,40 @@
 #include "bpf/bpf_helpers.h"
 #include "bpf/bpf_tracing.h"
 
+char LICENSE[] SEC("license") = "GPL";
+
+struct generic_file_open_args {
+	struct file *filp;
+	struct inode *inode;
+};
+
 SEC("kprobe/generic_file_open")
-int generic_file_open(struct pt_regs *ctx) {
-	bpf_printk("hit");
+int generic_file_open(struct generic_file_open_args *ctx) {
+	int pid = bpf_get_current_pid_tgid() >> 32;
+	const char fmt_str[] = "detected file open - pid: %d, file: %s, uid: %d\n";
+
+	// Read data over from kernel space
+	struct inode *inode;
+	struct file *filp;
+
+	bpf_probe_read(&inode, sizeof(inode), &ctx->inode);
+	bpf_probe_read(&filp, sizeof(filp), &ctx->filp);
+
+	// Read data over from user space
+	struct path *path;
+	struct dentry *dentry;
+	struct qstr d_name = {};
+
+	bpf_probe_read(&path, sizeof(path), &filp->f_path);
+	bpf_probe_read(&dentry, sizeof(dentry), &path->dentry);
+	bpf_probe_read(&d_name, sizeof(d_name), &dentry->d_name);
+
+	bpf_trace_printk(fmt_str, 
+			sizeof(fmt_str), 
+			pid, 
+			d_name.name,
+			&inode
+	);
+
 	return 0;
 }
